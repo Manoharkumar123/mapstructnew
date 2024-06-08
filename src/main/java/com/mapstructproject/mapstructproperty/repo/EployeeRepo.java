8bdac80aa1a11f17dceaf9dcffa0658e20ed8715
Manoharkumar123 <kumarmanohar161642@gmail.com>
2024-06-08 15:57:05 +0530

    changed empployeeName

--- src/main/java/com/mapstructproject/mapstructproperty/repo/EployeeRepo.java
+++ src/main/java/com/mapstructproject/mapstructproperty/repo/EployeeRepo.java
@@ -12,7 +12,7 @@ import com.mapstructproject.mapstructproperty.model.Employee;
 @Component
 public class EployeeRepo {
 	   EmpAddress address1=new EmpAddress("sudamdih","828126","Dhanbad");
-	   EmpAddress address2=new EmpAddress("kamrajnager","6000666","Chennai");
+	   EmpAddress address2=new EmpAddress("rajesh","6000666","Chennai");
 	   Employee emp1=new Employee("1","sanjay","clork","vechile",address1);
 	   Employee emp2=new Employee("2","manoj","teacher","arts",address2);
 	   List<Employee> employeelist= Arrays.asList(emp1,emp2);
